36e1e68dc81785b064cb85ca65a501fc987bfbe3
rae <rae@localhost.localdomain>
2014-01-11 15:16:49 -0800

    made scripts executable, removed git from the install script, since it already has to be installed to clone... oops!

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-sudo yum -y install fish gcc vim gvim git clang eclipse golang
+sudo yum -y install fish gcc vim gvim clang eclipse golang
 sudo yum -y install wine
 #programming tools installed.
 #Don't forget to install pipelight to run silverlight programs.
