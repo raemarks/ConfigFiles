f3fd199bcaa2a0eac66108093b271b5dddca84eb
root <root@localhost.localdomain>
2014-10-09 14:10:05 -0700

    updated config to have absolute paths

--- setup_config.sh
+++ setup_config.sh
@@ -1,8 +1,8 @@
 #! /bin/bash
-ln -s ~/ConfigFiles/.fonts ~/.fonts
-ln -s ~/ConfigFiles/.gvimrc ~/.gvimrc
-ln -s ~/ConfigFiles/.vimrc ~/.vimrc
-ln -s ~/ConfigFiles/fish ~/.config/fish
-ln -s ~/ConfigFiles/.gvimrc /root/.gvimrc
-ln -s ~/ConfigFiles/.vimrc /root/.vimrc
+ln -s /home/rmarks/ConfigFiles/.fonts /home/rmarks/.fonts
+ln -s /home/rmarks/ConfigFiles/.gvimrc /home/rmarks/.gvimrc
+ln -s /home/rmarks/ConfigFiles/.vimrc /home/rmarks/.vimrc
+ln -s /home/rmarks/ConfigFiles/fish /home/rmarks/.config/fish
+ln -s /home/rmarks/ConfigFiles/.gvimrc /root/.gvimrc
+ln -s /home/rmarks/ConfigFiles/.vimrc /root/.vimrc
 #manually copy the fstab and hosts over. Just copy the lines I need. 
