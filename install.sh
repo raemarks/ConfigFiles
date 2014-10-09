9fd5376583d1ed5212df71e352727f1ab5d414f9
root <root@localhost.localdomain>
2014-10-09 14:28:15 -0700

    removed eclipse and wine

--- install.sh
+++ install.sh
@@ -1,7 +1,6 @@
 #! /bin/bash
-sudo yum -y install fish gcc vim gvim clang eclipse golang
+sudo yum -y install fish gcc vim gvim clang golang
 sudo yum -y install gparted
-sudo yum -y install wine
 #programming tools installed.
 #Don't forget to install pipelight to run silverlight programs.
 #Install chrome.
