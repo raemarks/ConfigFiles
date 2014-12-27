4f1a62c959c1b0e30e6c661c1e8efa2758a37864
Raeanne Marks <rmarks@localhost.localdomain>
2014-12-26 18:35:08 -0800

    Added a couple programs to install

--- install.sh
+++ install.sh
@@ -1,8 +1,6 @@
 #! /bin/bash
-sudo yum -y install fish gcc vim gvim clang golang
+sudo yum -y install fish gcc vim gvim clang golang htop spot gnome-tweak-tool
 sudo yum -y install gparted
 #programming tools installed.
-#Don't forget to install pipelight to run silverlight programs.
 #Install chrome.
-#install the official jdk
 
